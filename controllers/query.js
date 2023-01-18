a34c38bfdb44bff89a785f3c6fef8c69776011e7
branhickman <branhickman@gmail.com>
2023-01-18 10:00:07 -0700

    add ability to capture userName's when adding shows from a search query - allowing the community shows to have a 'added by' datapoint

--- controllers/query.js
+++ controllers/query.js
@@ -1,5 +1,6 @@
 // will need to import models at some point
 import axios from 'axios'
+import { Profile } from '../models/profile.js'
 
 function index(req, res) {
   res.render('query/index', {
@@ -9,17 +10,21 @@ function index(req, res) {
 
 
 function search(req, res) {
-  axios.get(`https://api.themoviedb.org/3/search/tv?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1&query=${req.body.query}&include_adult=false`)
-  .then(response => {
-    console.log(response.data)
-    const data = response.data 
-    return data
-  })
-  .then ((data) => {
-    res.render('query/index', {
-      results: data.results,
-      title: "Search Results",
-      profile: req.params.id 
+  Profile.findById(req.body.profileId)
+  .then(profileData => {
+    axios.get(`https://api.themoviedb.org/3/search/tv?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1&query=${req.body.query}&include_adult=false`)
+    .then(response => {
+      console.log(response.data)
+      const data = response.data 
+      return data
+    })
+    .then ((data) => {
+      res.render('query/index', {
+        results: data.results,
+        title: "Search Results",
+        profile: req.params.id,
+        profileData: profileData,
+      })
     })
   })
 }
