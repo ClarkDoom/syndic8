1027f74e982595935b03869ab02561cc1ff9c3d2
branhickman <branhickman@gmail.com>
2023-01-20 09:23:32 -0700

    review indentation

--- routes/query.js
+++ routes/query.js
@@ -5,9 +5,10 @@ import { isLoggedIn } from '../middleware/middleware.js'
 const router = Router()
 
 router.get('/', isLoggedIn, queryCtrl.index)
-router.post('/', isLoggedIn, queryCtrl.search)
 router.get('/:id', isLoggedIn, queryCtrl.showSearch)
 router.get('/:id/:season', isLoggedIn, queryCtrl.seasonSearch)
+
+router.post('/', isLoggedIn, queryCtrl.search)
 
 export {
   router
--- routes/shows.js
+++ routes/shows.js
@@ -9,16 +9,10 @@ router.get('/watchlist', isLoggedIn, showCtrl.watchlist)
 router.get('/currently-watching', isLoggedIn, showCtrl.currentlyWatching)
 router.get('/seen-it', isLoggedIn, showCtrl.seenIt)
 router.get('/:id', isLoggedIn, showCtrl.show)
-
-
-
 router.get('/:id/:reviewId', isLoggedIn, showCtrl.showReview)
-
 router.get('/:id/:reviewId/edit', isLoggedIn, showCtrl.editReview)
 
 router.post('/new', isLoggedIn, showCtrl.create)
-
-
 router.post('/:id/reviews', isLoggedIn, showCtrl.createReview)
 router.post('/:id/:reviewId/add-comment', isLoggedIn, showCtrl.createComment)
 
